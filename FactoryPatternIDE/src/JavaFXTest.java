db3f2d09d9edb86266b960219e96d7c2f9ae3d15
Dan Christensen <816.dan@gmail.com>
2018-04-11 09:12:51 -0600

    JavaFX window now has a dropdown selector.

--- FactoryPatternIDE/src/JavaFXTest.java
+++ FactoryPatternIDE/src/JavaFXTest.java
@@ -1,7 +1,8 @@
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.ComboBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -14,16 +15,23 @@ public class JavaFXTest {
     }
 
     public void start(Stage primaryStage) throws Exception {
+        ComboBox box = new ComboBox();
+        box.getItems().addAll(
+                "Java",
+                "HTML");
+        box.getSelectionModel().selectFirst();
+
         Button button = new Button("BUTTON");
         button.setOnAction(e -> {
             Alert alert = new Alert(Alert.AlertType.INFORMATION);
             alert.setTitle("Alert.");
             alert.setHeaderText(null);
-            alert.setContentText("This is an alert.");
+            alert.setContentText("The selected item is: " + box.getValue());
             alert.showAndWait();
         });
-        StackPane root = new StackPane();
+        VBox root = new VBox();
         root.getChildren().add(button);
+        root.getChildren().add(box);
 
         Scene scene = new Scene(root, 800, 600);
 
