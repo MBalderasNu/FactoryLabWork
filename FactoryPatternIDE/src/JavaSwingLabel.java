8264c338086362c5ae56965ca9364574a278f151
zpeterson22 <zachrp2296@yahoo.com>
2018-04-13 08:41:31 -0600

    Added classes for swing button and label

---
+++ FactoryPatternIDE/src/JavaSwingLabel.java
@@ -0,0 +1,14 @@
+
+public class JavaSwingLabel extends UIComponent{
+
+	public JavaSwingLabel(int Height, int Width, int XPosition, int YPosition, String content) {
+		super(Height, Width, XPosition, YPosition, content);
+	}
+
+	@Override
+	public String getComponent() {
+		
+		return null;
+	}
+
+}
